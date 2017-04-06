Changed word classiciation data, added weighting of JCP-S dims, fixed initialization calculation, implemented PPMI shifting

embedding_benchmarks/scripts/web/evaluate.py
@@ -359,7 +359,7 @@ def evaluate_similarity(w, X, y):
     return scipy.stats.spearmanr(scores, y).correlation
 
 
-def evaluate_on_all(w):
+def evaluate_on_all(w, categorization=True):
     """
     Evaluate Embedding on all fast-running benchmarks
 
@@ -413,32 +413,33 @@ def evaluate_on_all(w):
     logger.info("Analogy prediction accuracy on {} {}".format("SemEval2012", analogy_results["SemEval2012_2"]))
     '''
     
-    '''
-    # Calculate results on categorization
-    logger.info("Calculating categorization benchmarks")
-    categorization_tasks = {
-        "AP": fetch_AP(),
-        "BLESS": fetch_BLESS(),
-        "Battig": fetch_battig(),
-        "ESSLI_2c": fetch_ESSLI_2c(),
-        "ESSLI_2b": fetch_ESSLI_2b(),
-        "ESSLI_1a": fetch_ESSLI_1a()
-    }
+    if categorization:
+        # Calculate results on categorization
+        logger.info("Calculating categorization benchmarks")
+        categorization_tasks = {
+            "AP": fetch_AP(),
+            "BLESS": fetch_BLESS(),
+            "Battig": fetch_battig(),
+            "ESSLI_2c": fetch_ESSLI_2c(),
+            "ESSLI_2b": fetch_ESSLI_2b(),
+            "ESSLI_1a": fetch_ESSLI_1a()
+        }
 
-    categorization_results = {}
+        categorization_results = {}
 
-    # Calculate results using helper function
-    for name, data in iteritems(categorization_tasks):
-        categorization_results[name] = evaluate_categorization(w, data.X, data.y)
-        logger.info("Cluster purity on {} {}".format(name, categorization_results[name]))
+        # Calculate results using helper function
+        for name, data in iteritems(categorization_tasks):
+            categorization_results[name] = evaluate_categorization(w, data.X, data.y)
+            logger.info("Cluster purity on {} {}".format(name, categorization_results[name]))
 
-    # Construct pd table
-    cat = pd.DataFrame([categorization_results])
+        # Construct pd table
+        cat = pd.DataFrame([categorization_results])
+
     #analogy = pd.DataFrame([analogy_results])
-    '''
     sim = pd.DataFrame([similarity_results])
     results = sim
-    #results = cat.join(sim).join(analogy)
-    #results = cat.join(sim)
+    if categorization:
+        results = cat.join(sim)
+    #results = cat.join(analogy)
 
     return results

embedding_evaluation.py
@@ -84,7 +84,7 @@ class EmbeddingTaskEvaluator(object):
         random.seed(42 + self.seed_bump)
 
     @lru_cache()
-    def get_word_classification_data(self, split_type='train'):
+    def get_word_classification_data_old(self, split_type='train'):
         words_and_POSs = []
         with open('evaluation_data/pos.txt') as f:
             for line in list(f):
@@ -109,13 +109,42 @@ class EmbeddingTaskEvaluator(object):
             X = sklearn.preprocessing.normalize(X)
         return X, y
 
+    @lru_cache()
+    def get_word_classification_data(self, split_type='train'):
+        words_and_emotions = []
+        with open('evaluation_data/emotions.txt') as f:
+            for line in list(f):
+                line = line.strip()
+                [word, emotion] = line.split(maxsplit=1)
+                if word in self.embedding_dict:
+                    words_and_emotions.append((word, emotion))
+        random.seed(42 + self.seed_bump)
+        random.shuffle(words_and_emotions)
+        deterministic_words = [(self.embedding_dict[word], cls) for (word, cls) in words_and_emotions if ' ' not in cls]  # Words with only one possible class
+        pos_neg_words = [(vec, cls) for (vec, cls) in deterministic_words if cls in ['emotion_positive', 'emotion_negative']]
+        deterministic_words = pos_neg_words
+        num_words = len(deterministic_words)
+        split_point = int(.85 * num_words)
+        if split_type == 'train':
+            data = deterministic_words[:split_point]
+        elif split_type == 'test':
+            data = deterministic_words[split_point:]
+        else:
+            raise ValueError('Unrecognized split type {}'.format(split_type))
+        X = np.array([x for (x,y_) in data])
+        y = np.array([y_ for (x,y_) in data])
+        if self.normalize_vects:
+            X = sklearn.preprocessing.normalize(X)
+        return X, y
+
     def word_classification_tasks(self, print_score=False):
         X, y = self.get_word_classification_data('train')
         X_test, y_test = self.get_word_classification_data('test')
 
-        LR = LogisticRegression()
-        LR.fit(X, y)
-        score = LR.score(X_test, y_test)
+        #classifier = LogisticRegression()
+        classifier = MLPClassifier(hidden_layer_sizes=(100, 42))
+        classifier.fit(X, y)
+        score = classifier.score(X_test, y_test)
         if print_score:
             print('Word classification score: {}'.format(score))
         with open('results/word_class_{}.txt'.format(self.method), 'w') as f:
@@ -366,10 +395,9 @@ class EmbeddingTaskEvaluator(object):
 
 
 if __name__ == '__main__':
-    method = 'svd'
+    method = 'nnse'
     evaluator = EmbeddingTaskEvaluator(method)
-    #evaluator.word_classification_tasks(print_score=True)
-    evaluator.analogy_tasks()
+    evaluator.word_classification_tasks(print_score=True)
     sys.exit()
     #score = evaluator.outlier_detection()
     evaluator.sentiment_classification_tasks(print_score=True)

tensor_decomp.py
@@ -289,6 +289,7 @@ class CPDecomp(object):
 
         print('initializing variables...')
         self.sess.run(tf.global_variables_initializer())
+        print("U: {}".format(self.U.eval(self.sess)))
         with self.sess.as_default():
             print('looping through batches...')
             for expected_indices, expected_values in expected_tensors:
@@ -386,7 +387,7 @@ class SymmetricCPDecomp(object):
                 mu = 10.0
             else:
                 mu = self.mean_value
-            mean = (1. / self.rank) * (mu ** (1/self.ndims))
+            mean = ((1. / self.rank) * mu) ** (1/self.ndims)
             self.U = tf.Variable(tf.random_normal(
                 shape=[dim, self.rank],
                 mean=mean,
@@ -575,7 +576,7 @@ class SymmetricCPDecomp(object):
 
 
 class JointSymmetricCPDecomp(SymmetricCPDecomp):
-    def __init__(self, size, rank, sess, dimlist=[2,3], reg_param=1e-10, nonneg=True, gpu=True):
+    def __init__(self, size, rank, sess, dimlist=[2,3], dimweights=[1., 1.], reg_param=1e-10, nonneg=True, gpu=True):
         '''
         `rank` is R, the number of 1D tensors to hold to get an approximation to `X`
         since X is supersymmetric, `size` is the length of each dimension
@@ -583,6 +584,8 @@ class JointSymmetricCPDecomp(SymmetricCPDecomp):
         Approximates a supersymmetric tensor whose approximations are repeatedly fed in batch format (indices always in sorted order) to `self.train`
         '''
         self.dimlist = dimlist
+        self.dimweights = dimweights
+        assert len(dimlist) == len(dimweights)
         self.rank = rank
         self.sess = sess
         self.nonneg = nonneg
@@ -603,12 +606,13 @@ class JointSymmetricCPDecomp(SymmetricCPDecomp):
                 self.X_ts.append(tf.SparseTensorValue(indices, values, shape=shape_sparse))
             # Goal: X_ijk == sum_{r=1}^{R} U_{ir} U_{jr} U_{kr}
             mu = 10.0
-            mean = (1. / self.rank) * (mu ** (1/3))
+            mean = ((1. / self.rank) * mu) ** (1/2)
             self.U = tf.Variable(tf.random_normal(
                 shape=[size, self.rank],
                 mean=mean,
                 stddev=mean / 5,
             ), name="U")
+            print('nonneg: {}'.format(self.nonneg))
             if self.nonneg:
                 self.sparse_U = tf.nn.relu(self.U, name='Sparse_U')
         self.create_loss_fn(reg_param=reg_param)
@@ -696,7 +700,7 @@ class JointSymmetricCPDecomp(SymmetricCPDecomp):
             self.reg = tf.constant(0.0)
         self.Ls = []
         for i, dim in enumerate(self.dimlist):
-            self.Ls.append(L(self.X_ts[i], U, dim))
+            self.Ls.append(self.dimweights[i] * L(self.X_ts[i], U, dim))
         self.L = sum(self.Ls)
         self.loss = self.L + self.reg
 

test_gensim.py
@@ -179,8 +179,10 @@ class GensimSandbox(object):
                 print('Dumping gatherer took {} secs'.format(time.time() - t))
         return gatherer
 
-    def train_joint_online_cp_embedding(self, dimlist: list, nonneg: bool):
+    def train_joint_online_cp_embedding(self, dimlist: list, dimweights: list, nonneg: bool):
         gatherers = [self.get_pmi_gatherer(dim) for dim in dimlist]
+        exp_shifts = [1., 5.]
+        shifts = [-np.log2(s) for s in exp_shifts]
 
         def sparse_tensor_batches(batch_size=1000):
             batches = batch_generator2(self.model, self.sentences_generator(num_sents=self.num_sents), batch_size=batch_size)
@@ -195,8 +197,9 @@ class GensimSandbox(object):
                         limit_large_vals=False,
                         neg_sample_percent=0.0,
                         pmi=True,
+                        shift=shift,
                     )
-                    for gatherer in gatherers
+                    for (shift, gatherer) in zip(shifts, gatherers)
                 ]
                 yield ([x[0] for x in pairlist], [x[1] for x in pairlist])
 
@@ -206,20 +209,21 @@ class GensimSandbox(object):
         self.sess = tf.Session(config=config)
         with self.sess.as_default():
             # random init: mean=(1. / self.embedding_dim) * (mu ** (1/ndims)). There will be ~|V|*k of these values.
-            mu = 10.
-            mean = (1. / self.embedding_dim) * (mu ** (1./3.))
+            mu = 20.
+            mean = (1. / self.embedding_dim) * (mu ** (1./4.))
             reg_param = mean / 100.  # ...heuristic
-            reg_param = 1e-8
+            reg_param = 1e-7
             self.to_save['reg_param'] = reg_param
             print('reg_param: {}'.format(reg_param))
             decomp_method = JointSymmetricCPDecomp(
                 size=len(self.model.vocab),
                 dimlist=dimlist,
+                dimweights=dimweights,
                 rank=self.embedding_dim,
                 sess=self.sess,
                 reg_param=reg_param,
                 nonneg=nonneg,
-                gpu=self.gpu,
+                gpu=True,
             )
         print('Starting JOINT CP Decomp training')
         decomp_method.train(sparse_tensor_batches())
@@ -233,6 +237,10 @@ class GensimSandbox(object):
 
     def train_online_cp_embedding(self, ndims: int, symmetric: bool, nonneg: bool):
         gatherer = self.get_pmi_gatherer(ndims)
+        if nonneg:
+            shift = 0.
+        else:
+            shift = -np.log2(15.)
 
         def sparse_tensor_batches(batch_size=1000, symmetric=symmetric):
             batches = batch_generator2(self.model, self.sentences_generator(num_sents=self.num_sents), batch_size=batch_size)
@@ -243,8 +251,9 @@ class GensimSandbox(object):
                     debug=False,
                     symmetric=symmetric,
                     log_info=False,
-                    neg_sample_percent=.25,
+                    neg_sample_percent=0.0 if nonneg else 0.05,
                     pmi=True,
+                    shift=shift,
                 )
                 yield sparse_ppmi_tensor_pair
 
@@ -256,7 +265,7 @@ class GensimSandbox(object):
         with self.sess.as_default():
             if symmetric:
                 print('getting full PMI tensor...')
-                (indices, values) = gatherer.create_pmi_tensor(positive=True, debug=False, symmetric=symmetric)
+                (indices, values) = gatherer.create_pmi_tensor(positive=True, debug=False, symmetric=symmetric, shift=shift)
                 mean_value = np.mean(values)
                 print('mean tensor value: {}'.format(mean_value))
 
@@ -276,7 +285,7 @@ class GensimSandbox(object):
                     optimizer_type='adam',
                     reg_param=reg_param,
                     nonneg=nonneg,
-                    gpu=self.gpu,
+                    gpu=False,
                     mean_value=mean_value,
                 )
             else:
@@ -341,10 +350,10 @@ class GensimSandbox(object):
             print('creating PPMI tensor...')
         else:
             print('creating sparse count tensor...')
-        indices, values = gatherer.create_pmi_tensor(positive=True, debug=True, symmetric=False, pmi=pmi)
-        matfile_name = 'sp_tensor_{}_{}.mat'.format(self.num_sents, self.min_count)
-        scipy.io.savemat(mat_filename, {'indices': indices, 'values': values})
-        print('saved {}. exiting.'.format(mat_filename))
+        indices, values = gatherer.create_pmi_tensor(positive=True, debug=True, symmetric=False, pmi=pmi, shift=-np.log2(15.))
+        matfile_name = 'sp_tensor_{}_{}_log15.mat'.format(self.num_sents, self.min_count)
+        scipy.io.savemat(matfile_name, {'indices': indices, 'values': values})
+        print('saved {}. exiting.'.format(matfile_name))
         sys.exit()
 
         from pymatbridge import Matlab
@@ -403,11 +412,11 @@ class GensimSandbox(object):
 
     def loadmatlab(self):
         #d = scipy.io.loadmat('../matlab/sp_tensor.mat')
-        d = scipy.io.loadmat('sp_tensor_10000000_2000.mat')
+        d = scipy.io.loadmat('sp_tensor_10000000_2000_log15.mat')
         values = d['values'].T
         indices = d['indices']
 
-        d = scipy.io.loadmat('../matlab/UVW_300_10e6_2000.mat')
+        d = scipy.io.loadmat('../matlab/UVW_300_10e6_2000_log15.mat')
         U = d['U']
         V = d['V']
         W = d['W']
@@ -501,12 +510,6 @@ class GensimSandbox(object):
         except Exception as e:
             print(e)
             print('caught exception trying to dump model. wooops. carrying on...')
-        try:
-            with open(parent_dir + '/sandbox.pkl', 'wb') as f:
-                dill.dump(self, f)
-        except Exception as e:
-            print(e)
-            print('caught exception trying to dump SANDBOX. carrying on...')
         for name, obj in self.to_save.items():
             fname = parent_dir + '/' + name + '.pkl'
             with open(fname, 'wb') as f:
@@ -541,10 +544,10 @@ class GensimSandbox(object):
             self.train_online_cp_embedding(ndims=4, symmetric=True, nonneg=True)
         elif self.method in ['jcp-s']:  # Joint Symmetric CP Decomp experiments
             self.method += experiment
-            self.train_joint_online_cp_embedding(dimlist=[2,3], nonneg=False)
+            self.train_joint_online_cp_embedding(dimlist=[2,3], dimweights=[1., .85,], nonneg=False)
         elif self.method in ['jcp-s_432']:
             self.method += experiment
-            self.train_joint_online_cp_embedding(dimlist=[2,3,4], nonneg=True)
+            self.train_joint_online_cp_embedding(dimlist=[2,3,4], dimweights=[2., .4, .1], nonneg=False)
         elif self.method in ['nnse']:
             self.method += experiment
             self.train_online_cp_embedding(ndims=2, symmetric=True, nonneg=True)
@@ -562,6 +565,10 @@ class GensimSandbox(object):
             self.restore_from_ckpt()
         else:
             raise ValueError('undefined method {}'.format(self.method))
+        for i, vec in enumerate(self.embedding):
+            if np.linalg.norm(vec) == 0.0:
+                self.embedding[i][0] += 1e-4
+
         self.evaluate_embedding()
         self.save_metadata()
         print('All done training and evaluating {}!'.format(self.method))